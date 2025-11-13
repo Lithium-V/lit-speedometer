9e4c8f6c577c02b8d2378918c0bb1fee99a412aa
Pedro Souza <pedrosouzadz6@pm.me>
2025-11-13 11:47:22 -0300

    feat(client): add function to hide HUD components
    Add hideHud function to hide specific HUD components for cleaner UI

--- client/index.ts
+++ client/index.ts
@@ -1,1 +1,10 @@
 import { event, toKm, vec3ToVelocity } from "./utils"
+
+function hideHud() {
+    // ---- [HIDE COMPONENTS] ---- 
+    HideHudComponentThisFrame(6) // VEHICLE NAME
+    HideHudComponentThisFrame(7) // AREA NAME
+    HideHudComponentThisFrame(8) // VEHICLE CLASS
+    HideHudComponentThisFrame(9) // STREET NAME  
+}
+
